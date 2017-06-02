ca7fb1958da6d5dd692fe3bfeb25016e39cd800a
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-06-02 16:16:32 +0300

    remove scroll from body when showing modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  private scrollBarWidth = 0;
 
   constructor(private renderer: Renderer2) { }
 
@@ -13,11 +14,19 @@ export class ModalComponent implements OnInit {
   }
 
   show() {
+    this.renderer.setStyle(document.body, 'padding-right', `${this.getScrollBarWidth()}px`);
     this.renderer.setStyle(document.body, 'overflow', 'hidden');
   }
 
   hide() {
     this.renderer.removeStyle(document.body, 'overflow');
+    this.renderer.removeStyle(document.body, 'padding-right');
   }
 
+  private getScrollBarWidth(): number {
+    if (this.scrollBarWidth === 0 && (document.body.scrollHeight > window.innerHeight)) {
+      this.scrollBarWidth  = window.innerWidth - document.body.clientWidth;
+    }
+    return this.scrollBarWidth;
+  }
 }
