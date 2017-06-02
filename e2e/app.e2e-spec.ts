c25c25780c80d1504e67f9b29b62f6d5ef184771
Angular CLI <angular-cli@angular.io>
2017-06-02 15:00:40 +0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { NgModal2Page } from './app.po';
+
+describe('ng-modal2 App', () => {
+  let page: NgModal2Page;
+
+  beforeEach(() => {
+    page = new NgModal2Page();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
