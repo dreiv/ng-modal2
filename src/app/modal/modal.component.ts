dfdf03b425db018e34cd33766fae73a5f338cf76
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-06-06 17:29:26 +0300

    very raw version with transition start and end.

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,37 +1,40 @@
-import {Component, ElementRef, HostBinding, OnDestroy, Renderer2} from '@angular/core';
+import {Component, ElementRef, HostBinding, OnDestroy, OnInit, Renderer2} from '@angular/core';
 import { DocumentService } from "../document.service";
 import { Subscription } from "rxjs/Subscription";
+import {Observable} from "rxjs/Observable";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements  OnDestroy {
-  private subscription: Subscription;
+export class ModalComponent implements OnDestroy {
+  private scrollbar$: Subscription;
+  private fade$: Subscription;
 
   @HostBinding('class.show')
   isShown: boolean;
+  @HostBinding('class.fade')
+  isFading: boolean;
 
   constructor(private renderer: Renderer2,
               private documentService: DocumentService,
               private element: ElementRef) { }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.scrollbar$.unsubscribe();
   }
 
   show() {
-    if(!this.isShown) {
+    if (!this.isShown) {
       this.handleScrollbar(this.documentService.verticalScrollbarWidth$.getValue());
       this.renderer.setStyle(document.body, 'overflow', 'hidden');
 
-      this.subscription = this.documentService.verticalScrollbarWidth$
-        .subscribe((scrollBarWidth)=> {
-          if(this.isShown) {
-            this.handleScrollbar(scrollBarWidth);
-          }
-        });
+      if(this.fade$) {
+        this.fade$.unsubscribe();
+      }
+      this.scrollbar$ = this.documentService.verticalScrollbarWidth$.subscribe((scrollBarWidth)=> { this.handleScrollbar(scrollBarWidth); });
 
       this.element.nativeElement.focus();
       this.isShown = true;
@@ -39,14 +42,21 @@ export class ModalComponent implements  OnDestroy {
   }
 
   hide() {
-    if(this.isShown) {
+    if (this.isShown) {
       this.renderer.removeStyle(document.body, 'padding-right');
       this.renderer.removeStyle(document.body, 'overflow');
 
-      this.subscription.unsubscribe();
+      if(this.scrollbar$) {
+        this.scrollbar$.unsubscribe();
+      }
 
       this.element.nativeElement.blur();
-      this.isShown = false;
+
+      this.isFading = true;
+      this.fade$ = Observable.fromEvent(this.element.nativeElement, 'transitionend').subscribe((e: Event) => {
+        this.isShown = false;
+        this.isFading = false;
+      });
     }
   }
 
